e421f0143ca869f66f3c10bcad522955388d24ba
rampavan90 <rampavan90@gmail.com>
2016-10-06 17:06:57 +0530

    First commit

---
+++ blog.js
@@ -0,0 +1,31 @@
+//var a = 2+3;
+(2+3);
+
+(3*7+2*5.0);
+
+//Immediately invoked Function expressions work----IIFE
+(
+  function(){
+
+var a = {
+ name:"Jens",
+ city:"Lund"
+
+
+};
+console.log(a);
+
+
+}
+)();
+
+// function Person(name,city){
+// console.log(name+" "+city);
+
+
+// }
+
+// //Explicit call to the function
+// Person("Jens","Lund");
+
+//console.log("hello");
